71ed2f6774e20954f618958afe584143d821166b
peterkitsakos <peter.kitsakos@gmail.com>
2021-10-05 17:10:50 -0400

    intitial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path')
+
+// This function initializes and loads a BrowserWindow of dimensions 1200x800px
+const loadMainWindow = () => {
+	const mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		center: true,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+
+	mainWindow.loadFile(path.join(__dirname, "index.html"))
+}
+
+// Once app is ready, run loadMainWindow function
+app.on("ready", loadMainWindow);
+
+app.on("windows-all-closed", () => {
+	if(process.platform !== "darwin"){
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if(BrowserWindow.getAllWindows() == 0){
+		loadMainWindow();
+	}
+});
---
+++ timer.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+
+	let timer = document.getElementById('timer');
+	console.log("loaded");
+	let timerMin = 20;
+	let timerSec = 11;
+	
+	setInterval(runTimer, 1000);
+
+	function runTimer() {
+		if(timerSec == 0){
+			timerMin--;
+			timerSec = 60;
+		}
+		timerSec--;
+		if(timerSec < 10){
+			timer.innerHTML = timerMin + ":0" + timerSec;
+		}else{
+			timer.innerHTML = timerMin + ":" + timerSec;
+		}
+	}
+}

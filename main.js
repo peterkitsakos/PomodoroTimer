227688289debe5bcec9259d0ed865deff53c330d
peterkitsakos <peter.kitsakos@gmail.com>
2021-10-08 12:02:44 -0400

    Beginning database implementation

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const {app, BrowserWindow} = require('electron');
-const path = require('path')
+const path = require('path');
+const mysql = require('mysql');
 
 // This function initializes and loads a BrowserWindow of dimensions 1200x800px
 const loadMainWindow = () => {
@@ -15,8 +16,24 @@ const loadMainWindow = () => {
 	mainWindow.loadFile(path.join(__dirname, "index.html"))
 }
 
+//Create MySQL Connection
+connectToDatabase = () => {
+	var connection = mysql.createConnection({
+		hostname: "localhost",
+		user: "peterkitsakos",
+		password: "chieftain11"
+	});
+
+	connection.connect(function(err){
+		if(err) throw error;
+		console.log("Connected!")
+	})
+}
+
+
 // Once app is ready, run loadMainWindow function
 app.on("ready", loadMainWindow);
+app.on("ready", connectToDatabase);
 
 app.on("windows-all-closed", () => {
 	if(process.platform !== "darwin"){
